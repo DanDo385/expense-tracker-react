22a535290523c3d06025d77539a1fbe88200504a
Daniel Magro <dando385@yahoo.com>
2023-10-11 14:01:35 -0400

    added UI components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import { Header } from './components/Header';
-import { Balance } from './components/Balance'; 
-import { IncomeExpenses } from './components/IncomeExpenses'; 
+import Header from './components/Header';  // This is for the default export
+import Balance from './components/Balance'; 
+import IncomeExpenses from './components/IncomeExpenses'; 
+import TransactionList from './components/TransactionList'; 
+import AddTransaction from './components/AddTransaction'; 
+
 import './App.css';
 
 function App() {
   return (
-    <>
+    <div>
       <Header />
-      <div className = "container">
-      <Balance />
-    </>
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+
+      </div>
+    </div>
   );
 }
 
 export default App;
+
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@
 export const Header = () => {
   return (
     <h2>Header</h2>
-  )
+  );
 }
 
 export default Header;
